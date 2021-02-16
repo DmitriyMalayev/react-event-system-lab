62a0010f5f8d7b85ac9e3973955c095487a2e93e
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-02-16 18:32:23 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,37 @@
-// Code EyesOnMe Component Here
+import React, { Component } from "react";
+
+class EyesOnMe extends Component {
+  handleFocus = () => {
+    console.log("Good!");
+  };
+
+  handleBlur = () => {
+    console.log("Hey! Eyes on me!");
+  };
+
+
+  render() {
+    return (
+      <button onFocus={this.handleFocus} onBlur={this.handleBlur}></button>
+    );
+  }
+}
+
+export default EyesOnMe;
+
+
+
+//   render() {
+//     return (
+//       <canvas
+//         onClick={toggleCycling}
+//         onMouseMove={this.handleMouseMove}
+//         onKeyPress={this.handleKeyPress}
+//         // onKeyPress={resize}
+//         width="900"
+//         height="600"
+//         tabIndex="0"
+//       ></canvas>
+//     );
+//   }
+// }
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
-// Code Keypad Component Here
+import React, { Component } from "react";
+
+class Keypad extends Component {
+//   handleKeyPress(event) {
+//     if (event.type === keyUp) {
+//       console.log("Entering password");
+//     }
+//   }
+
+  render() {
+    return (
+      <div>
+        <input type="password" onKeyUp={()=> console.log("Entering password...")} />
+      </div>
+    );
+  }
+}
+
+export default Keypad;
+
+
